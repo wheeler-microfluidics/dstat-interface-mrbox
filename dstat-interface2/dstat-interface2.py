72d32a91390dff7958ca25c44111c78a2a2ab64d
Michael Dryden <mdryden@chem.utoronto.ca>
2014-04-18 03:23:47 -0400

    Cleanup

--- dstat-interface2/dstat-interface2.py
+++
@@ -1,8 +0,0 @@
-"""
-" dstat-interface2.py
-" dstat-interface2
-"
-" Created by Michael Dryden on 2013-05-24.
-" Copyright 2013 Wheeler Lab. All rights reserved.
-"""
-# write your python code below
