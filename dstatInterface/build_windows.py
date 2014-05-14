ba7371556f0ac761a352e8c4e1a42f7dd1facd25
Michael Dryden <mdryden@chem.utoronto.ca>
2014-05-14 02:09:16 -0400

    Changed build_windows.py to work outside of project directory.

--- dstatInterface/build_windows.py
+++ dstatInterface/build_windows.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 __requires__ = 'PyInstaller==2.1'
 
-import PyInstaller.main as pyi
 
+import os, sys
+os.chdir(os.path.dirname(sys.argv[0]))
+
+import PyInstaller.main as pyi #For some reason, it gets the path here, so working dir must be set first
 pyi.run(['interface_test.spec'])
