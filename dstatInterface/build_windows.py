00c2072f2f5dcfa8538de00f1549d4ff7c382531
Michael D. M. Dryden <mdryden@chem.utoronto.ca>
2014-08-08 18:26:20 -0400

    build_windows.py passes command line arguments to pyinstaller.

--- dstatInterface/build_windows.py
+++ dstatInterface/build_windows.py
@@ -5,5 +5,8 @@ __requires__ = 'PyInstaller==2.1'
 import os, sys
 os.chdir(os.path.dirname(sys.argv[0]))
 
+args = ['interface_test.spec']
+args.extend(sys.argv[1:])
+
 import PyInstaller.main as pyi #For some reason, it gets the path here, so working dir must be set first
-pyi.run(['interface_test.spec'])
+pyi.run(args)
