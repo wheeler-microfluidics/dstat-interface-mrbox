b5899a83db0bd6da564d5c26886a4b13bd88c5b9
Michael D. M. Dryden <mdryden@chem.utoronto.ca>
2014-08-26 14:52:22 -0400

    Condense exp_window.py attributes into dicts.

--- dstat-interface/dstat-interface/interface/exp_window.py
+++ dstat-interface/dstat-interface/interface/exp_window.py
@@ -10,28 +10,26 @@ class Experiments:
     def __init__(self, builder):
         self.builder = builder
         
-        self.chronoamp = chronoamp.chronoamp()
-        self.lsv = lsv.lsv()
-        self.cve = cv.cv()
-        self.swv = swv.swv()
-        self.dpv = dpv.dpv()
-        self.acv = acv.acv()
-        self.pde = pd.pd()
+        self.classes = {}
+        self.classes['cae'] = chronoamp.Chronoamp()
+        self.classes['lsv'] = lsv.lsv()
+        self.classes['cve'] = cv.cv()
+        self.classes['swv'] = swv.swv()
+        self.classes['dpv'] = dpv.dpv()
+        self.classes['acv'] = acv.acv()
+        self.classes['pde'] = pd.pd()
  
         #fill exp_section
-        self.exp_section = self.builder.get_object('exp_section_box')
-        self.containers = {'cae': self.chronoamp.builder.get_object(
-                                                            'scrolledwindow1')}
-        self.containers['lsv'] = self.lsv.builder.get_object('scrolledwindow1')
-        self.containers['cve'] = self.cve.builder.get_object('scrolledwindow1')
-        self.containers['swv'] = self.swv.builder.get_object('scrolledwindow1')
-        self.containers['dpv'] = self.dpv.builder.get_object('scrolledwindow1')
-        self.containers['acv'] = self.acv.builder.get_object('scrolledwindow1')
-        self.containers['pde'] = self.pde.builder.get_object('scrolledwindow1')
+        exp_section = self.builder.get_object('exp_section_box')
+        self.containers = {}
+        
+        for key, cls in self.classes.iteritems():
+            self.containers[key] = cls.builder.get_object('scrolledwindow1')
 
         for key in self.containers:
-            self.containers[key].reparent(self.exp_section)
+            self.containers[key].reparent(exp_section)
             self.containers[key].hide()
+            
         
     def set_exp(self, selection):
         """Changes parameter tab to selected experiment. Returns True if 
@@ -46,3 +44,4 @@ class Experiments:
         self.containers[selection].show()
         
         return True
+        
